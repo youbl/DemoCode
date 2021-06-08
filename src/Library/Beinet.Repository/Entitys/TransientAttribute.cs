656a759894582459687d71a80c95bd3438d0a2c6
youbl <youbl@126.com>
2021-06-08 23:13:04 +0800

    bug

--- src/Library/Beinet.Repository/Entitys/TransientAttribute.cs
+++ src/Library/Beinet.Repository/Entitys/TransientAttribute.cs
@@ -5,7 +5,7 @@ namespace Beinet.Repository.Entitys
     /// <summary>
     /// 表示非数据库定义的字段
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
     public class TransientAttribute : Attribute
     {
     }
