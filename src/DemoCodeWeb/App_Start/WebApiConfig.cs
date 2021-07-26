87805b906d9210e17691aa81432bde8e91b22d9f
youbl <youbl@126.com>
2021-07-26 15:05:19 +0800

    添加注释说明

--- src/DemoCodeWeb/App_Start/WebApiConfig.cs
+++ src/DemoCodeWeb/App_Start/WebApiConfig.cs
@@ -21,6 +21,7 @@ namespace DemoCodeWeb
 
             // 添加一个URL处理类，处理 http://localhost/actuator/info 这种请求
             RouteTable.Routes.Add(new Route("actuator/info", new ActuatorInfoRouteHandler()));
+            // 在这里 RouteTable.Routes 与 config.Routes 是同步的，根据搜索的资料，RouteTable仅支持IIS，而config.Routes还支持非IIS，比如类似core的自启动
         }
     }
 }
