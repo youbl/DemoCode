62a51e0e8d39b03471e4619e7066a36f4657a89c
youbl <youbl@126.com>
2022-04-27 17:54:24 +0800

    添加可执行类和工具方法

---
+++ src/DemoCodeConsole/CoreTest/EnumHelperTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using Beinet.Core;
+using Beinet.Core.EnumExt;
+using Beinet.Core.FileExt;
+using Beinet.Core.Util;
+
+namespace DemoCodeConsole.CoreTest
+{
+    class EnumHelperTest : IRunable
+    {
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine(SystemHelper.GetCpuUsage());
+                Console.WriteLine(SystemHelper.GetMemoryTotal());
+                Console.WriteLine(SystemHelper.GetMemoryAvail());
+                Thread.Sleep(1200);
+            }
+
+            var aa = TestEnum.Aaa.GetDesc();
+            var bb = TestEnum.Bbb.GetDesc();
+            var cc = TestEnum.Ccc.GetDesc();
+        }
+    }
+
+    enum TestEnum
+    {
+        Aaa,
+        [Description("I'm xxx")] Bbb,
+
+        [Description] Ccc
+    }
+}
--- src/Library/Beinet.Core/IRunable.cs
+++ src/Library/Beinet.Core/IRunable.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Beinet.Core
+﻿namespace Beinet.Core
 {
     /// <summary>
     /// 带可执行方法的接口
